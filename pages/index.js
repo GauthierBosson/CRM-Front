9cc6daf033ec79eed8eedb9ebf5b107493b83f60
jeromeballueCSM <jerome.ballue@csmrouen.com>
2020-01-15 12:07:50 +0100

    modif accueil

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import LockOpenIcon from '@material-ui/icons/LockOpen';
-import NotificationsIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import Card from "@material-ui/core/Card";
+import CardContent from '@material-ui/core/CardContent';
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -22,6 +23,8 @@ const useStyles = makeStyles(theme => ({
   background: {
     height: "100vh",
     backgroundSize: "cover",
+    backgroundImage: 'radial-gradient(closest-corner,rgba(0, 28, 85,0) 60%,rgba(0, 28, 85,.5)),linear-gradient(to top left,#1F487E,#00ae5b 100%)'
+
 
   },
   submit: {
@@ -33,16 +36,18 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-      <div className={classes.background} style={{background:'linear-gradient(89.54755525118753deg, rgba(255, 255, 255,1) 1.249348965163032%,rgba(254, 254, 254,1) 1.249348965163032%,rgba(207, 207, 207,1) 100.18164388524987%)'}}>
+      <div className={classes.background}>
     <Container component="main" maxWidth="sm">
 
     <div style={{height:'100px'}} ></div>
       <CssBaseline />
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5" style={{fontSize:'40px'}}>
-          Connexion <LockOpenIcon style={{marginLeft:'6px', fontSize:'40px'}}/>
+        <Typography component="h1" variant="h5" style={{fontSize:'40px',color:'white'}}>
+          Connexion <LockOpenIcon style={{marginLeft:'6px', fontSize:'40px', color:'white'}}/>
         </Typography>
 
+        <Card style={{marginTop:'20px', backgroundColor:'#F1F1F1'}}>
+          <CardContent>
         <form className={classes.form} noValidate>
           <Grid style={{ marginTop: "20px" }}>
             <TextField
@@ -78,6 +83,9 @@ export default function SignIn() {
             </Button>
           </Grid>
         </form>
+          </CardContent>
+
+        </Card>
       </div>
     </Container>
       </div>
