e15967338172143a98c37b6e7b66502b341f8281
clement alllah <clement.allalah@smartpanda.com>
2020-01-28 16:03:59 +0100

    Ajout CSV et visualisation csv

--- components/Import.js
+++ components/Import.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { lighten, makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
@@ -6,6 +6,17 @@ import Grid from "@material-ui/core/Grid";
 import ArchiveIcon from "@material-ui/icons/Archive";
 import Button from "@material-ui/core/Button";
 import secondaryListItems from "../components/listItems";
+import ReactFileReader from "react-file-reader";
+import PublishIcon from "@material-ui/icons/Publish";
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+import TableHead from "@material-ui/core/TableHead";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import { Link } from "react-router-dom";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
+
 const useStyles = makeStyles(theme => ({
   root: {
     width: "100%"
@@ -38,11 +49,36 @@ export default function EnhancedTable() {
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(true);
   const [rowsPerPage, setRowsPerPage] = React.useState(7);
+  const [showMenuCsv, setShowMenuCsv] = useState(true);
+  const [showCsv, setShowCsv] = useState(false);
+  const [csvData, setCsvData] = useState([]);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
     setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
+  };
+  const handleCloseCsv = () => {
+    setShowCsv(false);
+  };
+
+  const handleFiles = files => {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      // Use reader.result
+      let csvResult = reader.result;
+      let ar = csvResult.split("\n");
+      delete ar[0];
+      setCsvData(ar);
+      console.log("ar", ar[1].split(",")[1]);
+      // console.log("ar", ar);
+      setShowCsv(true);
+    };
+    reader.readAsText(files[0]);
+  };
+
+  const handleMenuCsv = () => {
+    setShowMenuCsv(false);
   };
 
   return (
@@ -57,6 +93,7 @@ export default function EnhancedTable() {
                   Import fichier .csv ou Json
                 </h1>
               </div>
+
               <Grid container>
                 <Grid item xs={12}>
                   <Paper>
@@ -71,7 +108,15 @@ export default function EnhancedTable() {
                         <div align="center">
                           <div style={{ height: "250px" }}>
                             {" "}
-                            <Button color="primary"> Open Browser </Button>
+                            <ReactFileReader
+                              handleFiles={handleFiles}
+                              fileTypes={".csv"}
+                            >
+                              <Button variant="contained" color="primary">
+                                <PublishIcon />
+                                Import CSV
+                              </Button>
+                            </ReactFileReader>
                             <br />
                             <div align="center">
                               <ArchiveIcon
@@ -85,6 +130,104 @@ export default function EnhancedTable() {
                   </Paper>
                 </Grid>
               </Grid>
+
+              {showCsv && (
+                <div style={{ marginTop: "30px" }}>
+                  <Grid container direction="row" alignItems="center">
+                    <Grid item xs={12}>
+                      <h3 style={{ textAlign: "center" }}>Contenu du CSV</h3>
+                    </Grid>
+                  </Grid>
+                  <Grid container>
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={8} style={{ marginTop: "20px" }}>
+                      <Table>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell align="center">
+                              <h3>Prénom</h3>
+                            </TableCell>
+                            <TableCell align="center">
+                              <h3>Nom</h3>
+                            </TableCell>
+                            <TableCell align="center">
+                              <h3>Email</h3>
+                            </TableCell>
+                          </TableRow>
+                        </TableHead>
+
+                        <TableBody style={{ backgroundColor: "#e8f5e9" }}>
+                          {csvData
+                            .slice(
+                              page * rowsPerPage,
+                              page * rowsPerPage + rowsPerPage
+                            )
+                            .map(user => (
+                              <TableRow>
+                                <TableCell
+                                  align="center"
+                                  //   component={Link}
+                                  style={{ textDecoration: "none" }}
+                                >
+                                  <h6>{user.split(",")[1]}</h6>
+                                </TableCell>
+                                <TableCell
+                                  align="center"
+                                  //   component={Link}
+                                  style={{ textDecoration: "none" }}
+                                  id={user.id}
+                                >
+                                  <h6>{user.split(",")[2]}</h6>
+                                </TableCell>
+                                <TableCell
+                                  align="center"
+                                  //   component={Link}
+                                  style={{ textDecoration: "none" }}
+                                >
+                                  <h6>{user.split(",")[3]}</h6>
+                                </TableCell>
+                              </TableRow>
+                            ))}
+                        </TableBody>
+                      </Table>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                  </Grid>
+
+                  <Grid container direction="row" alignItems="center">
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={2}></Grid>
+                    <Grid
+                      item
+                      xs={2}
+                      style={{ textAlign: "center", marginTop: "20px" }}
+                    >
+                      <Button
+                        variant="contained"
+                        style={{ backgroundColor: "#2e7d32", color: "white" }}
+                        // onClick={handleAddCsv}
+                      >
+                        <CheckCircleIcon /> Valider CSV
+                      </Button>
+                    </Grid>
+                    <Grid
+                      item
+                      xs={2}
+                      style={{ textAlign: "center", marginTop: "20px" }}
+                    >
+                      <Button
+                        variant="contained"
+                        style={{ backgroundColor: "#d32f2f", color: "white" }}
+                        // onClick={handleCloseCsv}
+                      >
+                        <CancelPresentationIcon /> Annuler
+                      </Button>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={2}></Grid>
+                  </Grid>
+                </div>
+              )}
             </div>
           </Container>
         </React.Fragment>
