28a38afaa4b69d0c57368cad54e1dbf53edff01a
Gauthier <bosson.gauthier27@gmail.com>
2020-02-20 23:48:24 +0100

    change section

--- pages/homes.js
+++ pages/homes.js
@@ -311,16 +311,14 @@ function Dashboard() {
                   <Grid container>
                     <Grid item xs={6} style={{ marginTop: "10px" }}>
                       <Box justifyContent="center" style={{ display: "flex" }}>
-                        <Badge badgeContent={1} color="error">
-                          <AddAlertIcon
+                          <PeopleAltIcon
                             style={{ fontSize: "3rem", marginTop: "6px" }}
                           />
-                        </Badge>
                       </Box>
                     </Grid>
                     <Grid item xs={6} style={{ marginTop: "10px" }}>
                       <Box justifyContent="center" style={{ display: "flex" }}>
-                        <h3>Demandes</h3>
+                        <h3>Utilisateurs</h3>
                       </Box>
                     </Grid>
                   </Grid>
